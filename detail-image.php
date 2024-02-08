8a20fb6e4f74d04a255cdd6e7e39489e2ecb10fc
nova_rohman <novanrohman@gmail.com>
2024-02-08 15:00:11 +0700

    update alert login & comment

--- detail-image.php
+++ detail-image.php
@@ -18,7 +18,12 @@ if ($userLoggedIn && isset($_POST['like'])) {
     $admin_name = $_SESSION['a_global']->admin_name;
 
     // Check if the user already liked the image
-    $checkLiked = mysqli_query($conn, "SELECT * FROM tb_like WHERE image_id = '$imageId' AND admin_name = '$admin_name'");
+    if ($userLoggedIn) {
+        $checkLiked = mysqli_query($conn, "SELECT * FROM tb_like WHERE image_id = '" . $_GET['id'] . "' AND admin_name = '" . $_SESSION['a_global']->admin_name . "'");
+        $isLiked = (mysqli_num_rows($checkLiked) > 0);
+    } else {
+        $isLiked = false;
+    }
 
     if (mysqli_num_rows($checkLiked) > 0) {
         // Unlike if already liked
@@ -32,6 +37,19 @@ if ($userLoggedIn && isset($_POST['like'])) {
 // Get the total likes for the image
 $qt = mysqli_query($conn, "SELECT COUNT(*) AS total_likes FROM tb_like WHERE image_id = '" . $_GET['id'] . "'");
 $totalLikes = (mysqli_num_rows($qt) > 0) ? mysqli_fetch_array($qt)['total_likes'] : 0;
+
+// Handle comments
+if ($userLoggedIn && isset($_POST['submit_comment'])) {
+    $image_id = $_GET['id'];
+    $admin_id = $_SESSION['a_global']->admin_id;
+    $admin_name = $_SESSION['a_global']->admin_name;
+    $isi_komentar = mysqli_real_escape_string($conn, $_POST['komentar']);
+
+    mysqli_query($conn, "INSERT INTO komentar_foto (image_id, admin_id, admin_name, isi_komentar) VALUES ('$image_id', '$admin_id','$admin_name', '$isi_komentar')");
+}
+
+// Get comments for the image
+$commentQuery = mysqli_query($conn, "SELECT * FROM komentar_foto WHERE image_id = '" . $_GET['id'] . "'");
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -42,12 +60,13 @@ $totalLikes = (mysqli_num_rows($qt) > 0) ? mysqli_fetch_array($qt)['total_likes'
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>WEB Galeri Foto</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 </head>
 
 <body>
-     <!-- header -->
-     <header>
+    <!-- header -->
+    <header>
         <div class="container">
         <h1><a href="index.php">WEB GALERI FOTO</a></h1>
         <ul>
@@ -88,28 +107,39 @@ $totalLikes = (mysqli_num_rows($qt) > 0) ? mysqli_fetch_array($qt)['total_likes'
             </div>
 
             <div class="col-7">
-                <!-- Like Button -->
-                <?php if ($userLoggedIn) : ?>
-                    <form action="" method="POST">
-                        <button type="submit" name="like" class="like">Suka <?php echo $totalLikes ?></button>
-                    </form>
-                <?php else : ?>
-                    <p>Login to like the image</p>
-                <?php endif; ?>
-
                 <div class="content">
-                    <form action="" method="POST">
-                        <input type="hidden" name="adminid" value="<?php echo $adminId ?>">
-                        <textarea name="komentar" id="" type="text" cols="300" class="input-control" placeholder="Tulis Komentas..." required></textarea>
-                        <input type="submit" name="submit" value="Kirim" class="btn">
-                    </form>
-                    <?php
-                    if (isset($_POST['submit'])) {
-                        echo '<script>alert("Login Terlebih Dahulu")</script>';
-                        echo '<script>window.location="login.php"</script>';
-                    }
-                    ?>
-                    <br>
+                    <!-- Like Button -->
+                    <?php if ($userLoggedIn) : ?>
+                        <form action="" method="POST">
+                            <button type="submit" name="like" class="like" id="like-btn">
+                                <?php if ($isLiked == true) : ?>
+                                    <i class="bi bi-suit-heart-fill"></i> 
+                                <?php else : ?>
+                                    <i class="bi bi-suit-heart"></i> 
+                                <?php endif; ?>
+                                <?php echo $totalLikes ?>
+                            </button>
+                        </form>
+                    <?php else : ?>
+                        <button class="like" onclick="showLoginAlert()">
+                            <i class="bi bi-suit-heart"></i> Like
+                        </button>
+                    <?php endif; ?>
+
+                    <!-- Comment Form -->
+                    
+                        <form action="" method="POST">
+                            <input type="hidden" name="adminid" value="<?php echo $_SESSION['a_global']->admin_id ?>">
+                            <textarea name="komentar" id="" type="text" cols="300" class="input-control" placeholder="Tulis Komentar..." required></textarea>
+                            <input type="submit" onclick="showLoginAlert()" name="submit_comment" value="Kirim Komentar" class="btn">
+                        </form>
+                   
+
+                    <!-- Display Comments -->
+                    <h4>Komentar:</h4>
+                    <?php while ($comment = mysqli_fetch_object($commentQuery)) : ?>
+                        <p><strong><?php echo $comment->admin_name; ?>:</strong> <?php echo $comment->isi_komentar; ?></p>
+                    <?php endwhile; ?>
                 </div>
             </div>
         </div>
@@ -122,8 +152,23 @@ $totalLikes = (mysqli_num_rows($qt) > 0) ? mysqli_fetch_array($qt)['total_likes'
         </div>
     </footer>
 
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script>
-        // You can include any additional scripts if needed
+        // Dynamically update the like button appearance
+        // $(document).ready(function() {
+        //     $("#like-btn").click(function() {
+        //         $(this).find("i").toggleClass("bi-suit-heart-fill");
+        //         $(this).text(function(i, text) {
+        //             return text === "Like" ? "Unlike" : "Like";
+        //         });
+        //     });
+        // });
+
+        // Function to show login alert
+        function showLoginAlert() {
+            alert("Login Terlebih Dahulu");
+            window.location="login.php";
+        }
     </script>
 </body>
 
